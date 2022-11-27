e0054049efb4b97930a83ccf4db3c4fb4ce19d2b
Grzegorz <g.malisz@student.fontys.nl>
2022-11-27 18:08:42 +0100

    Shop and _Layout design

---
+++ WebApp/Pages/Shop.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages
+{
+    public class ShopModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
