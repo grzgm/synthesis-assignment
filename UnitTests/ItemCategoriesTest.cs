859ee9c19209f6aac105af990971d92dda4b0933
Grzegorz Malisz <g.malisz@student.fontys.nl>
2022-12-13 16:41:38 +0100

    ItemCategories tests

---
+++ UnitTests/ItemCategoriesTest.cs
@@ -0,0 +1,42 @@
+﻿using LogicLayer.InterfacesManagers;
+using LogicLayer.InterfacesRepository;
+using LogicLayer.Managers;
+using LogicLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+	[TestClass]
+	public class ItemCategoryTest
+	{
+		private IItemCategoryRepository itemCategoryRepository;
+		private IItemCategoryManager itemCategoryManager;
+
+		[TestMethod]
+		public void ReadItemCategoryConvertionFromDTOToCategoryAndSubCategory()
+		{
+			// Arrange
+			itemCategoryRepository = new FakeItemCategoryDB();
+			itemCategoryManager = new ItemCategoryManager(itemCategoryRepository);
+			List<ItemCategory> itemCategories;
+			List<ItemCategory> itemSubCategories;
+			int assertCounter = 0;
+
+			// Act
+			(itemCategories, itemSubCategories) = itemCategoryManager.ReadAllItemCategories();
+
+			// Assert
+			foreach (ItemCategory itemSubCategory in itemSubCategories)
+			{
+				if(itemSubCategory.ParentCategory == itemCategories[0])
+				assertCounter++;
+			}
+			Assert.AreEqual(assertCounter, 2);
+
+		}
+	}
+}
