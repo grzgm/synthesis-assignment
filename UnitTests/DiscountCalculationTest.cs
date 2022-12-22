841df769b1b4b7664ddfd1533998f7e721a8035a
Grzegorz Malisz <g.malisz@student.fontys.nl>
2022-12-22 01:20:53 +0100

    Exeption unit tests

--- UnitTests/DiscountCalculationTest.cs
+++ UnitTests/DiscountCalculationTest.cs
@@ -39,5 +39,31 @@ namespace UnitTests
 			// Assert
 			Assert.AreEqual(discountValue, 25);
 		}
+
+		// Assert
+		[ExpectedException(typeof(DivideByZeroException))]
+		[TestMethod]
+		public void DiscountFixedCalculateDiscountExceptionTest()
+		{
+			// Arrange
+			IDiscount discount = new DiscountFixed(10, 0, DateTime.Now, DateTime.Now, "test");
+			decimal discountValue;
+
+			// Act
+			discountValue = discount.CalculateDiscount(5, 10);
+		}
+
+		// Assert
+		[ExpectedException(typeof(DivideByZeroException))]
+		[TestMethod]
+		public void DiscountPercentageCalculateDiscountExceptionTest()
+		{
+			// Arrange
+			IDiscount discount = new DiscountPercentage(50, 0, DateTime.Now, DateTime.Now, "test");
+			decimal discountValue;
+
+			// Act
+			discountValue = discount.CalculateDiscount(7, 10);
+		}
 	}
 }
